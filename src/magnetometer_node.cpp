bcb3146f9fb44a76f3a26bd7267894824cd92089
Eric Foss <ericadamfoss@gmail.com>
2024-06-09 23:51:35 +0000

    Working filtered dmp compass

--- src/magnetometer_node.cpp
+++ src/magnetometer_node.cpp
@@ -6,6 +6,8 @@
 #include <robotcontrol.h>
 
 #define I2C_BUS 2
+#define GPIO_INT_PIN_CHIP 3
+#define GPIO_INT_PIN_PIN 21
 
 using namespace std::chrono_literals;
 
@@ -17,7 +19,7 @@ public:
         angle_publisher_ = this->create_publisher<std_msgs::msg::Float64>("magnetometer_angle", 10);
 
         timer_ = this->create_wall_timer(
-            500ms, std::bind(&MagnetometerNode::read_magnetometer_data, this));
+            50ms, std::bind(&MagnetometerNode::read_magnetometer_data, this));
 
         // Initialize the robot control library
         if (rc_initialize() != 0) {
@@ -29,12 +31,16 @@ public:
 
 	rc_mpu_config_t conf = rc_mpu_default_config();
 	conf.i2c_bus = I2C_BUS;
+	conf.gpio_interrupt_pin_chip = GPIO_INT_PIN_CHIP;
+	conf.gpio_interrupt_pin = GPIO_INT_PIN_PIN;
 	conf.enable_magnetometer = 1;
+	conf.dmp_sample_rate = 100;
         // Initialize the magnetometer
-        if (rc_mpu_initialize(&mpu_data_, conf) != 0) {
+        if (rc_mpu_initialize_dmp(&mpu_data_, conf) != 0) {
             RCLCPP_FATAL(this->get_logger(), "Failed to initialize MPU");
             rclcpp::shutdown();
         }
+
 
         RCLCPP_INFO(this->get_logger(), "Magnetometer node has been initialized");
     }
@@ -49,24 +55,31 @@ public:
 private:
     void read_magnetometer_data()
     {
-        if (rc_mpu_read_mag(&mpu_data_) != 0) {
-            RCLCPP_WARN(this->get_logger(), "Failed to read magnetometer data");
-            return;
-        }
+        //if (rc_mpu_read_mag(&mpu_data_) != 0) {
+        //    RCLCPP_WARN(this->get_logger(), "Failed to read magnetometer data");
+        //    return;
+        //}
 
-        // Extract x and y magnetometer data
+        // Extract magnetometer data
         double mag_x = mpu_data_.mag[0];
         double mag_y = mpu_data_.mag[1];
+	double mag_z = mpu_data_.mag[2];
+	double heading = mpu_data_.compass_heading;
 
         // Compute the angle of the y-axis with respect to north
-        double angle = atan2(mag_y, mag_x);
+        double angle = atan2(mag_x, mag_y);
+	angle = 180 * angle / 3.141592;
+
+	// Convert Filtered Heading to y axis
+	heading = 180 * heading / 3.141592;
+	heading = heading + 90.0;
 
         // Publish the angle
         auto message = std_msgs::msg::Float64();
         message.data = angle;
         angle_publisher_->publish(message);
 
-        RCLCPP_INFO(this->get_logger(), "Published angle: %.2f radians", angle);
+        RCLCPP_INFO(this->get_logger(), "Angle: %.2f, Filtered Heading: %.2f", angle, heading);
     }
 
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr angle_publisher_;
